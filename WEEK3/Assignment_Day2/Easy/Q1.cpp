757e879383e5d0eedf66c99f2bf8c9bdd0cefb70
SARA-collar <saraswotink@gmail.com>
2026-01-25 20:18:27 +0545

    Add Week 3 assignment

--- WEEK3/Assignment_Day2/Easy/Q1.cpp
+++ WEEK3/Assignment_Day2/Easy/Q1.cpp
@@ -1,19 +1,18 @@
-/*
-Solid Square of Stars
-*/
-
 #include <iostream>
 using namespace std;
 
 int main() {
     int n;
-    cout << "Enter size of square: ";
+    cout << "Enter the size of the square: ";
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++)
+    // Outer loop for rows
+    for (int i = 0; i < n; i++) {
+        // Inner loop for columns
+        for (int j = 0; j < n; j++) {
             cout << "*";
-        cout << endl;
+        }
+        cout << endl; // Move to next row
     }
 
     return 0;
